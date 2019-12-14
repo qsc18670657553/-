f028bdbb76df5105130e761ad8f877db3f8cbf5c
qsc <29151691889@qq.com>
2019-12-14 13:57:41 +0800

    dev 1.0

--- gmall-test/src/main/java/com/atguigu/gmall/test/Controller.java
+++ gmall-test/src/main/java/com/atguigu/gmall/test/Controller.java
@@ -3,5 +3,6 @@ package com.atguigu.gmall.test;
 public class Controller {
     public static void main(String[] args) {
         System.out.println("laowan");
+        System.out.println("1");
     }
 }
