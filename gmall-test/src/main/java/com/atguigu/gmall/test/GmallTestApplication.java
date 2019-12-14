1b616a541c84cade0eefd68588de37c8b17e80c7
qsc <29151691889@qq.com>
2019-12-14 10:41:38 +0800

    dev 1.0

---
+++ gmall-test/src/main/java/com/atguigu/gmall/test/GmallTestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallTestApplication.class, args);
+	}
+
+}
